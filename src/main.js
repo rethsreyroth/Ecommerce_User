c9a178a16a36fd6bd4a8a88207023d8b60c49b46
Maisivheng <hengyagami@gmail.com>
2026-05-15 12:18:02 +0700

    add bootstrap

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +12,4 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
