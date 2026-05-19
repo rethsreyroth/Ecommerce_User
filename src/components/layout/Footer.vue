ae11a1f2ebb53a285539deb83530a8f6f9f469e5
Reth Sreyroth <roth64165@gmail.com>
2026-05-20 00:17:30 +0700

    HomePage(new navbar&footer)

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,61 +1,36 @@
 <template>
-    <footer class="py-4 Footer">
-		<div class="container">
-			<div class="row d-flex justify-content-between align-items-center">
-				<div class="col-12 col-lg-3 pe-lg-0 mb-4 mb-lg-0">
-					<h1 class="navbar-brand">ពិភពទំនិញ</h1>
-				</div>
-				<div class="col-12 col-lg-9 pe-lg-0 mb-4 mb-lg-0">
-					<div class="row d-flex justify-content-between align-items-start">
-						<div class="col-12 col-lg-3">
-							<h3 class="footer-title">Quick Links</h3>
-							<ul class="list-unstyled">
-								<li class="nav-item">
-									<router-link class="nav-link" to="/">ទំព័រដើម</router-link>
-								</li>
-								<li class="nav-item">
-									<router-link class="nav-link" to="/aboutUS">អំពីយើង</router-link>
-								</li>
-								<li class="nav-item">
-									<router-link class="nav-link" to="/products">ផលិតផល</router-link>
-								</li>
-								<li class="nav-item">
-									<router-link class="nav-link" to="/contactUS">ទំនាក់ទំនង</router-link>
-								</li>
-							</ul>
-						</div>
-						<div class="col-12 col-lg-3">
-							<h3 class="footer-KHtitle">ព័ត៌មានទំនាក់ទំនង</h3>
-							<ul class="list-unstyled">
-								<li class="nav-item">
-									<router-link class="nav-link" to="/"><i class="bi bi-envelope me-2"></i>Example@gmail.com</router-link>
-								</li>
-								<li class="nav-item">
-									<router-link class="nav-link" to="/"><i class="bi bi-telephone me-2"></i>+855 XX XXX XXX</router-link>
-								</li>
-								<li class="nav-item">
-									<router-link class="nav-link" to="/"><i class="bi bi-geo-alt me-2"></i>ភ្នំពេញ, កម្ពុជា</router-link>
-								</li>
-							</ul>
-						</div>
-						<!-- <div class="col-12 col-lg-1"></div> -->
-						<div class="col-12 col-lg-3">
-							<h3 class="footer-title">Follow us</h3>
-							<ul class="list-unstyled">
-								<li class="nav-item">
-									<router-link class="nav-link" to="/"><i class="bi bi-facebook me-2"></i>Facebook</router-link>
-								</li>
-								<li class="nav-item">
-									<router-link class="nav-link" to="/"><i class="bi bi-twitter-x me-2"></i>Twitter</router-link>
-								</li>
-								<li class="nav-item">
-									<router-link class="nav-link" to="/"><i class="bi bi-instagram me-2"></i>Instagram</router-link>
-								</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</footer>
+	<footer>
+        <div class="container">
+            <div class="row g-4 g-lg-5">
+                <div class="col-12 col-md-3">
+                    <div class="footer-title mb-3">ពិភពទំនិញ</div>
+                    <p class="text-muted">រាល់ការទិញទំនិញបច្ចេកវិទ្យាប្រកបដោយទំនុកចិត្ត និងគុណភាពខ្ពស់។</p>
+                </div>
+                <div class="col-12 col-sm-6 col-md-3">
+                    <h6 class="footer-title mb-3">ព័ត៌មានទំនាក់ទំនង</h6>
+                    <ul class="list-unstyled text-muted d-grid gap-2">
+                        <li><i class="bi bi-envelope me-2"></i> Example@gmail.com</li>
+                        <li><i class="bi bi-telephone me-2"></i> +855 XX XXX XXX</li>
+                        <li><i class="bi bi-geo-alt me-2"></i> Phnom Penh, Cambodia</li>
+                    </ul>
+                </div>
+                <div class="col-12 col-sm-6 col-md-3">
+                    <h6 class="footer-title mb-3">Quick Links</h6>
+                    <ul class="list-unstyled d-grid gap-2">
+                        <li><router-link to="/aboutUs" class="text-decoration-none text-muted">អំពីយើង</router-link></li>
+                        <li><router-link to="/products" href="#" class="text-decoration-none text-muted">ផលិតផល</router-link></li>
+                        <li><router-link to="/" class="text-decoration-none text-muted">សេវាកម្ម</router-link></li>
+                    </ul>
+                </div>
+                <div class="col-12 col-md-3">
+                    <h6 class="footer-title mb-3">Follow us</h6>
+                    <div class="d-flex gap-3">
+                        <router-link to="/" class="text-dark fs-5"><i class="bi bi-facebook"></i></router-link>
+                        <router-link to="/" class="text-dark fs-5"><i class="bi bi-twitter-x"></i></router-link>
+                        <router-link to="/" class="text-dark fs-5"><i class="bi bi-instagram"></i></router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 </template>
--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,44 +1,34 @@
 <template>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid justify-content-between">
-            <a class="navbar-brand" href="#">ពិភពទំនិញ</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm sticky-top">
+        <div class="container-fluid">
+            <a class="navbar-brand me-4" href="#">ពិភពទំនិញ</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse justify-content-between" id="navbarSupportedContent">  
-                <form class="search-input-wrapper">
-                    <i class="bi bi-search"></i>
-                    <input type="text" placeholder="ស្វែងរក" class="search-input" autofocus>
-                </form>
-
-                <div class="btn-router d-flex align-items-center justify-content-between gap-2">
-                    <router-link class="nav-link btn-item d-flex justify-content-center" to="/"><i class="bi bi-cart3 me-1"></i>ទិញ</router-link>
-                    <router-link class="nav-link btn-item d-flex justify-content-center" to="/"><i class="bi bi-shop me-1"></i>លក់</router-link>
-
-                    <ul class="navbar-nav align-items-center">
-                        <li class="nav-item">
-                            <router-link class="nav-link" to="/">ទំព័រដើម</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link class="nav-link" to="/aboutUS">អំពីយើង</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link class="nav-link" to="/products">ផលិតផល</router-link>
-                        </li>
-                        <li class="nav-item">
-                            <router-link class="nav-link" to="/contactUS">ទំនាក់ទំនង</router-link>
-                        </li>
-                    </ul>
-                </div>  
+            <div class="collapse navbar-collapse" id="navbarNav">
                 
-                <div class=" d-flex justify-content-between align-items-center gap-3">
-                    <router-link class="nav-link cart-link px-2 py-1 rounded-5 d-flex justify-content-center" to="/"><i class="bi bi-cart3"></i><span class="text-danger">3</span>កន្រ្តក</router-link>
-                    <router-link to="/"><i class="bi bi-person-circle"></i></router-link>
+                <div class="search-wrapper col-12 col-lg-3 my-2 my-lg-0 me-3">
+                    <i class="bi bi-search"></i>
+                    <input class="form-control" type="search" placeholder="ស្វែងរក" aria-label="Search">
                 </div>
+
+                <div class="d-flex gap-2 my-2 my-lg-0 me-auto">
+                    <a href="#" class="btn-nav-outline"><i class="bi bi-cart"></i> ចង់ទិញ</a>
+                    <a href="#" class="btn-nav-outline"><i class="bi bi-shop"></i> ចង់លក់</a>
+                </div>
+
+                <ul class="navbar-nav align-items-center gap-3">
+                    <li class="nav-item"><a class="nav-link text-dark" href="#">ទំព័រដើម</a></li>
+                    <li class="nav-item"><a class="nav-link text-dark" href="#">អំពីយើង</a></li>
+                    <li class="nav-item"><a class="nav-link text-dark" href="#">ផលិតផល</a></li>
+                    <li class="nav-item me-2"><a class="nav-link text-dark" href="#">ទំនាក់ទំនង</a></li>
+                    <li class="nav-item d-flex gap-3 ms-2">
+                        <a href="#" class="text-dark fs-5"><i class="bi bi-cart"></i></a>
+                        <a href="#" class="text-dark fs-5"><i class="bi bi-person"></i></a>
+                    </li>
+                </ul>
             </div>
         </div>
     </nav>
-
     
-
 </template>
